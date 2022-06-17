ee5f2d59314b7ff846dc52eef3abe27c5a96382a
gabbyprecious <gabbyprecious2000@gmail.com>
2022-06-17 09:00:49 +0200

    rpc: implementing the rpc basic blocks for UNIX socket

--- client/unix.go
+++ client/unix.go
@@ -1,16 +1,32 @@
 package client
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
 	"net"
 )
 
+type Request struct {
+	Method  string                 `json:"method"`
+	Params  map[string]interface{} `json:"params"`
+	Jsonrpc string                 `json:"jsonrpc"`
+	Id      int                    `json:"id"`
+}
+
+type Response struct {
+	// Result string `json:result`
+	Error   string `json:"error"`
+	Jsonrpc string `json:jsonrpc`
+	Id      int    `json:"id"`
+}
 type UnixRPC struct {
-	socket net.Listener
+	socket net.Conn
 }
 
 func NewUnix(path string) (*UnixRPC, error) {
-	socket, err := net.Listen("unix", "<PATH>")
+	socket, err := net.Dial("unix", path)
 	if err != nil {
 		return nil, err
 	}
@@ -19,6 +35,48 @@ func NewUnix(path string) (*UnixRPC, error) {
 	}, nil
 }
 
-func (instance UnixRPC) Call(method string, payload map[string]any) ([]byte, error) {
-	return nil, fmt.Errorf("To be implemented")
+func EncodeToBytes(p interface{}) []byte {
+	buf := bytes.Buffer{}
+	enc := json.NewEncoder(&buf)
+	err := enc.Encode(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("uncompressed size (bytes): ", len(buf.Bytes()))
+	return buf.Bytes()
+}
+
+func DecodeToResponse(s []byte) *Response {
+	r := Response{}
+	dec := json.NewDecoder(bytes.NewReader(s))
+	err := dec.Decode(&r)
+	fmt.Print(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &r
+}
+
+func (instance UnixRPC) Call(data Request) (*Response, error) {
+	//change request to bytes
+	dataBytes := EncodeToBytes(data)
+	log.Printf(string(dataBytes))
+	//send data
+	_, err := instance.socket.Write(dataBytes)
+
+	if err != nil {
+		return nil, err
+	}
+	//read response
+	recvData := make([]byte, 1024)
+	bytesResp1, err := instance.socket.Read(recvData[:])
+	fmt.Print(string(recvData[:bytesResp1]))
+
+	if err != nil {
+		return nil, err
+	}
+	//decode response
+	resp := DecodeToResponse(recvData[:bytesResp1])
+	log.Print(resp)
+	return resp, nil
 }
--- client/unix_test.go
+++ client/unix_test.go
@@ -1,6 +1,9 @@
 package client
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"testing"
 )
 
@@ -9,16 +12,27 @@ type GetInfo struct {
 }
 
 func TestUnixCallOne(t *testing.T) {
-	// TODO take the path from the os environment
-	client, err := NewUnix("<PATH>")
+	path := os.Getenv("CLN_UNIX_SOCKET")
+	if path == "" {
+		err := fmt.Errorf("Unix path not exported with the CLN_UNIX_SOCKET env variable")
+		panic(err)
+	}
+
+	client, err := NewUnix(path)
 	if err != nil {
 		panic(err)
 	}
-	getInfo, err := Call[*UnixRPC, GetInfo](client, "getinfo", make(map[string]any))
+	request := Request{Method: "getinfo", Params: make(map[string]interface{}), Jsonrpc: "2.0", Id: 0}
+	response, err := client.Call(request)
 	if err != nil {
 		panic(err)
 	}
-	if getInfo.id == "" {
-		panic("")
+	if response == nil {
+		panic("The get info is null, there is some problem with the client implementation")
 	}
+	log.Print(response)
+	if request.Method != "getinfo" {
+		panic("method is not a getinfo command")
+	}
+	// TODO: make an assertion on the part of what the request contains
 }
