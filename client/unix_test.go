f7da37b6057d3d406d3af26c5162c8a925c3c34d
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-06-22 22:42:32 +0100

    rpc: bug fixing and implement the generic call
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- client/unix_test.go
+++ client/unix_test.go
@@ -2,13 +2,12 @@ package client
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"testing"
 )
 
 type GetInfo struct {
-	id string
+	Id string `json:"id"`
 }
 
 func TestUnixCallOne(t *testing.T) {
@@ -22,13 +21,43 @@ func TestUnixCallOne(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	response, err := client.Call( "getinfo", make(map[string]interface{}))
+	response, err := client.Call("getinfo", make(map[string]interface{}))
+
 	if err != nil {
 		panic(err)
 	}
+
 	if response == nil {
 		panic("The get info is null, there is some problem with the client implementation")
 	}
-	log.Print(response)
-	// TODO: make an assertion on the part of what the request contains
+
+	if response["id"] == "" {
+		panic("Response received by the node is invalid")
+	}
+}
+
+func TestUnixCallOneTyped(t *testing.T) {
+	path := os.Getenv("CLN_UNIX_SOCKET")
+	if path == "" {
+		err := fmt.Errorf("Unix path not exported with the CLN_UNIX_SOCKET env variable")
+		panic(err)
+	}
+
+	client, err := NewUnix(path)
+	if err != nil {
+		panic(err)
+	}
+	response, err := Call[UnixRPC, GetInfo](client, "getinfo", make(map[string]interface{}))
+
+	if err != nil {
+		panic(err)
+	}
+
+	if response == nil {
+		panic("The get info is null, there is some problem with the client implementation")
+	}
+
+	if response.Id == "" {
+		panic("Response received by the node is invalid")
+	}
 }
