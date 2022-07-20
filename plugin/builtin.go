bd6d9f87d25648ab7acb9354ae047303be97af85
gabbyprecious <gabbyprecious2000@gmail.com>
2022-07-20 14:41:51 +0100

    dragt getmanifest method

--- plugin/builtin.go
+++ plugin/builtin.go
@@ -1,10 +1,19 @@
 package plugin
+import (
+	"encoding/json"
+)
 
 type getManifest[T any] struct{}
 
 func (instance *getManifest[T]) Call(plugin *Plugin[T], request map[string]any) (map[string]any, error) {
 	result := make(map[string]any)
 	// TODO: feel the getmanifest result
+	result["options"], _ = json.Marshal(plugin.Options)
+	result["rpcmethods"], _ = json.Marshal(plugin.rpcMethods)
+	result["hooks"], _ = json.Marshal(plugin.hooks)
+	result["subscriptions"], _ = json.Marshal(plugin.subscriptions)
+	result["notifications"], _ = json.Marshal(plugin.notifications);
+	result["dynamic"] = plugin.dynamic;
 	return result, nil
 }
 
--- plugin/command.go
+++ plugin/command.go
@@ -1,10 +1,10 @@
 package plugin
 
 type rpcMethod[T any] struct {
-	name            string
-	usage           string
+	name            string 
+	usage           string 
 	description     string
-	longDescription string
+	LongDescription string `json:"long_description"`
 	callback        RPCCommand[T]
 }
 
@@ -18,5 +18,16 @@ type rpcNotification[T any] struct {
 }
 
 func (instance *rpcNotification[T]) Call(plugin *Plugin[T], request map[string]any) {
+	instance.callback.Call(plugin, request)
+}
+
+type rpcHook[T any] struct {
+	name 	  	string 
+	before 	   	[]string 
+	after 	   	[]string 
+	callback  	RPCCommand[T] 
+}
+
+func (instance *rpcHook[T]) Call(plugin *Plugin[T], request map[string]any) {
 	instance.callback.Call(plugin, request)
 }
