a59f9a465f10d844980834dd4ea1090fa5ca8e6e
gabbyprecious <gabbyprecious2000@gmail.com>
2022-08-12 20:32:13 +0100

    fix:option return value, add test

--- plugin/example/simple_plugin.go
+++ plugin/example/simple_plugin.go
@@ -17,11 +17,23 @@ func (instance *Hello[PluginState]) Call(plugin *plugin.Plugin[PluginState], req
 	return map[string]any{"message": "hello from go 1.18"}, nil
 }
 
+type GetFooOption[T PluginState] struct{}
+
+func (instance *GetFooOption[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) (map[string]any, error) {
+	BarValue, state := plugin.GetOpt("foo")
+
+	if state != true {
+		BarValue = ""
+	}
+	return map[string]any{"message": BarValue}, nil
+}
+
 func main() {
 	state := PluginState{}
 	plugin := plugin.New(&state, true, nil)
 	plugin.RegisterOption("foo", "string", "Hello Go", "An example of option", false)
 	plugin.RegisterRPCMethod("hello", "", "an example of rpc method", &Hello[PluginState]{})
+	plugin.RegisterRPCMethod("foo_bar", "", "an example of rpc method", &GetFooOption[PluginState]{})
 	plugin.RegisterNotification("rpc_command", &OnRPCCommand[PluginState]{})
 	plugin.Start()
 }
--- plugin/plugin_test.go
+++ plugin/plugin_test.go
@@ -27,3 +27,24 @@ func TestCallFistMethod(t *testing.T) {
 		t.Errorf("message received %s different from expected %s", message, "hello from go 1.18")
 	}
 }
+
+func TestOptionValueExist(t *testing.T) {
+	path := os.Getenv("CLN_UNIX_SOCKET")
+	client, err := client.NewUnix(path)
+	if err != nil {
+		panic(err)
+	}
+	response, err := client.Call("foo_bar", make(map[string]interface{}))
+	if err != nil {
+		panic(err)
+	}
+
+	message, found := response["message"]
+	if !found {
+		t.Error("The message is not found")
+	}
+
+	if message != "Hello Go" {
+		t.Errorf("message received %s different from expected %s", message, "Hello Go")
+	}
+}
