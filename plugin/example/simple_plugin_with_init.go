20776c93cf2f1f7a976dac3ad8a12bf1975f5782
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-08-14 21:07:10 +0100

    example(plugin): refactoring plugin example
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/example/simple_plugin_with_init.go
+++ plugin/example/simple_plugin_with_init.go
@@ -4,22 +4,22 @@ import (
 	"github.com/vincenzopalazzo/cln4go/plugin"
 )
 
-type PluginState struct{}
+type PluginStateInit struct{}
 
-type OnRPCCommand[T PluginState] struct{}
+type OnRPCCommandInit[T PluginStateInit] struct{}
 
-func (instance *OnRPCCommand[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) {
+func (instance *OnRPCCommandInit[PluginStateInit]) Call(plugin *plugin.Plugin[PluginStateInit], request map[string]any) {
 }
 
-type Hello[T PluginState] struct{}
+type HelloInit[T PluginStateInit] struct{}
 
-func (instance *Hello[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) (map[string]any, error) {
+func (instance *HelloInit[PluginStateInit]) Call(plugin *plugin.Plugin[PluginStateInit], request map[string]any) (map[string]any, error) {
 	return map[string]any{"message": "hello from go 1.18"}, nil
 }
 
-type GetFooOption[T PluginState] struct{}
+type GetFooOptionInit[T PluginStateInit] struct{}
 
-func (instance *GetFooOption[PluginState]) Call(plugin *plugin.Plugin[PluginState], request map[string]any) (map[string]any, error) {
+func (instance *GetFooOptionInit[PluginStateInit]) Call(plugin *plugin.Plugin[PluginStateInit], request map[string]any) (map[string]any, error) {
 	BarValue, state := plugin.GetOpt("foo")
 
 	if state != true {
@@ -28,16 +28,17 @@ func (instance *GetFooOption[PluginState]) Call(plugin *plugin.Plugin[PluginStat
 	return map[string]any{"message": BarValue}, nil
 }
 
-func onInit(state PluginState, conf map[string]any) map[string]any {
+func onInit(state PluginStateInit, conf map[string]any) map[string]any {
 	return map[string]any{}
 }
 
+/// By module resolution this is imported inside the other plugin, but just to make the test happy
 func main() {
-	state := PluginState{}
+	state := PluginStateInit{}
 	plugin := plugin.New(&state, true, onInit)
 	plugin.RegisterOption("foov2", "string", "Hello Go", "An example of option", false)
-	plugin.RegisterRPCMethod("hellov2", "", "an example of rpc method", &Hello[PluginState]{})
-	plugin.RegisterRPCMethod("foo_barv2", "", "an example of rpc method", &GetFooOption[PluginState]{})
-	plugin.RegisterNotification("rpc_command", &OnRPCCommand[PluginState]{})
+	plugin.RegisterRPCMethod("hellov2", "", "an example of rpc method", &HelloInit[PluginStateInit]{})
+	plugin.RegisterRPCMethod("foo_barv2", "", "an example of rpc method", &GetFooOptionInit[PluginStateInit]{})
+	plugin.RegisterNotification("rpc_command", &OnRPCCommandInit[PluginStateInit]{})
 	plugin.Start()
 }
