578de3943498f0b4ff2e1a49378794062d06abcf
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-10-04 16:43:02 +0200

    feat(common): add encoder interface and go impl
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ comm/encoder/encoder.go
@@ -0,0 +1,38 @@
+package encoder
+
+import (
+	"encoding/json"
+)
+
+type JSONEncoder interface {
+	EncodeToByte(obj any) ([]byte, error)
+
+	EncodeToString(obj any) (*string, error)
+
+	DecodeFromString(jsonStr *string, obj any) error
+
+	DecodeFromBytes(jsonBytes []byte, obj any) error
+}
+
+type GoEncoder struct{}
+
+func (self *GoEncoder) EncodeToByte(obj any) ([]byte, error) {
+	return json.Marshal(obj)
+}
+
+func (self *GoEncoder) EncodeToString(obj any) (*string, error) {
+	jsonByte, err := json.Marshal(obj)
+	if err != nil {
+		return nil, err
+	}
+	jsonStr := string(jsonByte)
+	return &jsonStr, nil
+}
+
+func (self *GoEncoder) DecodeFromString(jsonStr *string, obj any) error {
+	return json.Unmarshal([]byte(*jsonStr), &obj)
+}
+
+func (self *GoEncoder) DecodeFromBytes(jsonByte []byte, obj any) error {
+	return json.Unmarshal(jsonByte, &obj)
+}
