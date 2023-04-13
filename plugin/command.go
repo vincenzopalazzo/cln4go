a26bfd990e6b1d3b8d2f8c47b9409fec68b23b3f
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-13 14:55:26 +0200

    feat(plugin): semplify the plugin API by using the callback directly
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/command.go
+++ plugin/command.go
@@ -1,35 +1,35 @@
 package plugin
 
 type rpcMethod[T any] struct {
-	Name            string        `json:"name"`
-	Usage           string        `json:"usage"`
-	Description     string        `json:"description"`
-	LongDescription string        `json:"long_description"`
-	callback        RPCCommand[T] `json:"-"`
+	Name            string                                                                  `json:"name"`
+	Usage           string                                                                  `json:"usage"`
+	Description     string                                                                  `json:"description"`
+	LongDescription string                                                                  `json:"long_description"`
+	callback        func(plugin *Plugin[T], request map[string]any) (map[string]any, error) `json:"-"`
 }
 
 func (instance *rpcMethod[T]) Call(plugin *Plugin[T], request map[string]any) (map[string]any, error) {
-	return instance.callback.Call(plugin, request)
+	return instance.callback(plugin, request)
 }
 
 type rpcNotification[T any] struct {
 	onEvent  string
-	callback RPCEvent[T]
+	callback func(plugin *Plugin[T], request map[string]any)
 }
 
 func (instance *rpcNotification[T]) Call(plugin *Plugin[T], request map[string]any) {
-	instance.callback.Call(plugin, request)
+	instance.callback(plugin, request)
 }
 
 type rpcHook[T any] struct {
 	name     string
 	before   []string
 	after    []string
-	callback RPCCommand[T]
+	callback func(plugin *Plugin[T], request map[string]any) (map[string]any, error)
 }
 
 func (instance *rpcHook[T]) Call(plugin *Plugin[T], request map[string]any) {
-	if _, err := instance.callback.Call(plugin, request); err != nil {
+	if _, err := instance.callback(plugin, request); err != nil {
 		plugin.tracer.Infof("%s", err)
 	}
 }
