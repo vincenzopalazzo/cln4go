9987394060343769ffe417db342483c35bb81885
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-13 12:10:06 +0200

    feat(bench): add benchmarks module
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ bench/go.mod
@@ -0,0 +1,29 @@
+module git.hedwing.dev/vincenzopalazzo/cln-rpc-benchmarks
+
+go 1.20
+
+require (
+	github.com/LNOpenMetrics/go-lnmetrics.reporter v0.0.6-rc2.0.20230218083049-db6e98c64f5e
+	github.com/charmbracelet/log v0.2.1
+	github.com/vincenzopalazzo/cln4go/client v0.0.0-20230131113223-c518122c3f70
+	github.com/vincenzopalazzo/glightning v0.8.3-0.20230405224222-e0556b8b5eb0
+)
+
+require (
+	github.com/LNOpenMetrics/lnmetrics.utils v0.1.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.7.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.1 // indirect
+	github.com/niftynei/glightning v0.8.2 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/vincenzopalazzo/cln4go/comm v0.0.0-20230131113223-c518122c3f70 // indirect
+	github.com/vincenzopalazzo/cpstl/go v0.0.0-20221204131531-d0659db4dd2a // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
---
+++ bench/main.go
@@ -0,0 +1,122 @@
+// core lightning RPC benchmarks
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/charmbracelet/log"
+
+	jsonv2 "github.com/LNOpenMetrics/go-lnmetrics.reporter/pkg/json"
+	"github.com/LNOpenMetrics/go-lnmetrics.reporter/pkg/model"
+	"github.com/vincenzopalazzo/glightning/glightning"
+
+	cln "github.com/vincenzopalazzo/cln4go/client"
+)
+
+// The benchmarks contains a sequence of the
+// bench.
+type benchResult struct {
+	Bench []*bench `json:"bench"`
+}
+
+type bench struct {
+	Name  string        `json:"name"`
+	Runs  int           `json:"runs"`
+	Times time.Duration `json:"times"`
+	TimeStr string 		`json:"time_str"`
+}
+
+// runBencmarks - entry point of the benchmarks!
+func runBenchmarks(benchs map[string]func(*testing.B)) {
+	result := benchResult{
+		Bench: []*bench{},
+	}
+
+	for name, bench_fn := range benchs {
+		res := testing.Benchmark(bench_fn)
+		b := bench{
+			Name:  name,
+			Runs:  res.N,
+			Times: res.T,
+			TimeStr: fmt.Sprintf("%s", res.T),
+		}
+		log.Infof("----------------- %s -----------------", name)
+		log.Infof("Number of run: %d", b.Runs)
+		log.Infof("Time taken: %s", b.Times)
+		result.Bench = append(result.Bench, &b)
+		log.Infof("Size result: %d", len(result.Bench))
+	}
+	strJson, err := json.Marshal(result)
+	if err != nil {
+		log.Errorf("%s", err)
+	}
+	log.Infof("json: %s", string(strJson))
+	_ = ioutil.WriteFile("bench.json", strJson, 0644)
+}
+
+var rpc *cln.UnixRPC
+var gln *glightning.Lightning
+
+func init() {
+	path := os.Getenv("CLN_UNIX_SOCKET")
+	if path == "" {
+		err := fmt.Errorf("Unix path not exported with the CLN_UNIX_SOCKET env variable")
+		panic(err)
+	}
+
+	rpc, _ = cln.NewUnix(path)
+	rpc.SetEncoder(&jsonv2.FastJSON{})
+
+	gln = glightning.NewLightning()
+	url := strings.Split(path, "/")
+	socket := url[len(url)-1]
+	subPath := url[0 : len(url)-1]
+	pathStr := strings.Join(subPath, "/")
+	gln.StartUp(socket, pathStr)
+}
+
+func main() {
+	bench := map[string]func(*testing.B){
+		"cln4go-listnodes":        cln4goListNodes,
+		"glightning-listnodes":    glightningListNodes,
+		"cln4go-listchannels":     cln4goListChannels,
+		"glightning-listchannels": glightningListChannels,
+	}
+	runBenchmarks(bench)
+}
+
+// plain call to list nodes
+func cln4goListNodes(b *testing.B) {
+	_, err := cln.Call[map[string]any, model.ListNodesResp](rpc, "listnodes", map[string]any{})
+	if err != nil {
+		log.Error("cln4go listnodes", err)
+	}
+}
+
+func glightningListNodes(b *testing.B) {
+	_, err := gln.ListNodes()
+	if err != nil {
+		log.Error("glightning listnodes", err)
+	}
+}
+
+// plain call to list nodes
+func cln4goListChannels(b *testing.B) {
+	_, err := cln.Call[map[string]any, *model.ListChannelsResp](rpc, "listchannels", map[string]any{})
+	if err != nil {
+		log.Error("cln4go listchannels", err)
+	}
+}
+
+func glightningListChannels(b *testing.B) {
+	_, err := gln.ListChannels()
+	if err != nil {
+		log.Error("glightning listchannels", err)
+	}
+}
