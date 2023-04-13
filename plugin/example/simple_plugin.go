bd519706d82ebe2bde1caa76ad0415cd558c599c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-13 14:59:21 +0200

    fix(plugin): update the plugin to use the new API
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/example/simple_plugin.go
+++ plugin/example/simple_plugin.go
@@ -13,35 +13,20 @@ type PluginState struct {
 	Name string
 }
 
-func (self *PluginState) GetName() *string {
-	return &self.Name
-}
+func OnRpcCommand(plugin *plugin.Plugin[*PluginState], request map[string]any) {}
 
-func (self *PluginState) SetName(name string) {
-	self.Name = name
-}
-
-type OnRPCCommand[T IPluginState] struct{}
-
-func (instance *OnRPCCommand[IPluginState]) Call(plugin *plugin.Plugin[IPluginState], request map[string]any) {
-}
-
-type Hello[T IPluginState] struct{}
-
-func (instance *Hello[IPluginState]) Call(plugin *plugin.Plugin[IPluginState], request map[string]any) (map[string]any, error) {
+func Hello(plugin *plugin.Plugin[*PluginState], request map[string]any) (map[string]any, error) {
 	return map[string]any{"message": "hello from go 1.18"}, nil
 }
 
-type GetFooOption[T IPluginState] struct{}
-
-func (instance *GetFooOption[IPluginState]) Call(plugin *plugin.Plugin[IPluginState], request map[string]any) (map[string]any, error) {
+func GetOption(plugin *plugin.Plugin[*PluginState], request map[string]any) (map[string]any, error) {
 	BarValue, state := plugin.GetOpt("foo")
-	plugin.State.SetName("cln4go-opt")
+	plugin.State.Name = "cln4go-opt"
 
 	if !state {
 		BarValue = ""
 	}
-	return map[string]any{"message": BarValue, "name": plugin.GetState().GetName()}, nil
+	return map[string]any{"message": BarValue, "name": plugin.GetState().Name}, nil
 }
 
 func main() {
@@ -50,8 +35,8 @@ func main() {
 	}
 	plugin := plugin.New(&state, true, plugin.DummyOnInit[*PluginState])
 	plugin.RegisterOption("foo", "string", "Hello Go", "An example of option", false)
-	plugin.RegisterRPCMethod("hello", "", "an example of rpc method", &Hello[*PluginState]{})
-	plugin.RegisterRPCMethod("foo_bar", "", "an example of rpc method", &GetFooOption[*PluginState]{})
-	plugin.RegisterNotification("rpc_command", &OnRPCCommand[*PluginState]{})
+	plugin.RegisterRPCMethod("hello", "", "an example of rpc method", Hello)
+	plugin.RegisterRPCMethod("foo_bar", "", "an example of rpc method", GetOption)
+	plugin.RegisterNotification("rpc_command", OnRpcCommand)
 	plugin.Start()
 }
