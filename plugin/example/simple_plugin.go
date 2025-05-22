6aa9d8b1912ca6152430adbecf1fb21d65cadef2
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2025-05-22 18:02:09 +0200

    plugin: add a way to return the json RPC error
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- plugin/example/simple_plugin.go
+++ plugin/example/simple_plugin.go
@@ -29,6 +29,13 @@ func GetOption(plugin *plugin.Plugin[*PluginState], request map[string]any) (map
 	return map[string]any{"message": BarValue, "name": plugin.GetState().Name}, nil
 }
 
+// Example of an RPC method returning a JSONRPCError for JSON-RPC 2.0 error propagation.
+// Note: If you get a 'plugin.JSONRPCError is not a type' error, ensure your import path is correct and
+// that JSONRPCError is exported from the plugin package (it is, as of the latest code).
+func ErrorExample(p *plugin.Plugin[*PluginState], request map[string]any) (map[string]any, error) {
+	return nil, plugin.MakeRPCError(1001, "This is a JSON-RPC error from Go", map[string]any{"hint": "You can add extra error data here"})
+}
+
 func main() {
 	state := PluginState{
 		Name: "cln4go",
@@ -37,6 +44,7 @@ func main() {
 	plugin.RegisterOption("foo", "string", "Hello Go", "An example of option", false)
 	plugin.RegisterRPCMethod("hello", "", "an example of rpc method", Hello)
 	plugin.RegisterRPCMethod("foo_bar", "", "an example of rpc method", GetOption)
+	plugin.RegisterRPCMethod("error_example", "", "an example of JSON-RPC error return", ErrorExample)
 	plugin.RegisterNotification("rpc_command", OnRpcCommand)
 	plugin.Start()
 }
