3eeed420fc555577e97e8af139052bd77284c312
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2026-02-04 18:20:04 +0100

    client: fix test assertions and unexported struct field
    Replace panic() with t.Fatal() for proper test failure reporting.
    Use t.Skip() when CLN_UNIX_SOCKET is unset so tests are skipped
    rather than crashing. Export GetInfo.Network with a json tag so
    json.Unmarshal can actually populate the field.

--- client/unix_test.go
+++ client/unix_test.go
@@ -2,7 +2,6 @@ package client
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"testing"
 )
@@ -10,58 +9,56 @@ import (
 func TestUnixCallOne(t *testing.T) {
 	path := os.Getenv("CLN_UNIX_SOCKET")
 	if path == "" {
-		err := fmt.Errorf("Unix path not exported with the CLN_UNIX_SOCKET env variable")
-		panic(err)
+		t.Skip("CLN_UNIX_SOCKET not set, skipping integration test")
 	}
 
 	client, err := NewUnix(path)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 	response, err := Call[map[string]any, map[string]any](client, "getinfo", map[string]any{})
 
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	if response == nil {
-		panic("The get info is null, there is some problem with the client implementation")
+		t.Fatal("getinfo returned nil response")
 	}
 
 	if response["id"] == "" {
 		resp, _ := json.Marshal(response)
-		panic(fmt.Sprintf("Response received by the node is invalid: %s", string(resp)))
+		t.Fatalf("response missing id field: %s", string(resp))
 	}
 }
 
 type MapReq = map[string]any
 type GetInfo struct {
-	network string
+	Network string `json:"network"`
 }
 
 func TestUnixCallOneTyped(t *testing.T) {
 	path := os.Getenv("CLN_UNIX_SOCKET")
 	if path == "" {
-		err := fmt.Errorf("Unix path not exported with the CLN_UNIX_SOCKET env variable")
-		panic(err)
+		t.Skip("CLN_UNIX_SOCKET not set, skipping integration test")
 	}
 
 	client, err := NewUnix(path)
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	response, err := Call[MapReq, *GetInfo](client, "getinfo", make(map[string]any, 0))
 
 	if err != nil {
-		panic(err)
+		t.Fatal(err)
 	}
 
 	if response == nil {
-		panic("The get info is null, there is some problem with the client implementation")
+		t.Fatal("getinfo returned nil response")
 	}
 
-	if response.network == "bitcoin" {
-		panic("the network should be different from the bitcoin one")
+	if response.Network == "" {
+		t.Fatal("expected non-empty network field in getinfo response")
 	}
 }
